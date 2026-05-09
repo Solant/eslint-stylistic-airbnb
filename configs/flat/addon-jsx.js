1021d666c0325ff2955520bd82788cfff8f9ed1b
Solant <runner62v6@gmail.com>
2026-05-10 00:58:57 +0200

    fix: replace deprecated jsx-indent rule with indent (#9)

--- configs/flat/addon-jsx.js
+++ configs/flat/addon-jsx.js
@@ -25,8 +25,6 @@ module.exports = {
       prop: 'parens-new-line',
     }],
     '@stylistic/jsx-self-closing-comp': 'error',
-    // deprecated, but still needed for a proper formatting
-    '@stylistic/jsx-indent': ['error', 2],
     '@stylistic/jsx-indent-props': ['error', 2],
     '@stylistic/jsx-max-props-per-line': ['error', { maximum: 1, when: 'multiline' }],
     '@stylistic/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
